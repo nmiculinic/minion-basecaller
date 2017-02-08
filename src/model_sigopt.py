3bcd6a97b8406d914d319a7859776a3b00969859
Neven Miculinic <neven.miculinic@gmail.com>
2017-02-09 00:02:17 +0100

    SIGOPT bounds experiment
    It seems sigopt has troubles with linear inequalities....oh well

--- src/model_sigopt.py
+++ src/model_sigopt.py
@@ -19,20 +19,20 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, **kwarg
 
     with tf.name_scope("model"):
         print("model in", net.get_shape())
-        for j in range(3):
-            with tf.variable_scope("block%d" % (j + 1)):
-                for i in range(kwargs['b%d_layers' % (j + 1)]):
-                    with tf.variable_scope('layer_%d' % (i + 1)):
+        for block in range(1, 4):
+            with tf.variable_scope("block%d" % block):
+                for layer in range(1, 1 + kwargs['b%d_layers' % block]):
+                    with tf.variable_scope('layer_%d' % layer):
                         net = conv_1d(
                             net,
-                            2**kwargs['lower_lg'],
-                            1 + 2 * kwargs['receptive_field_l'],
+                            2**kwargs['l%d_lower_lg' % block],
+                            1 + 2 * kwargs['l%d_receptive_field_l' % block],
                             scope='conv_1d_lower'
                         )
                         net = conv_1d(
                             net,
-                            2**kwargs['upper_lg'],
-                            1 + 2 * kwargs['receptive_field_u'],
+                            2**kwargs['l%d_upper_lg' % block],
+                            1 + 2 * kwargs['l%d_receptive_field_u' % block],
                             scope='conv_1d_upper'
                         )
                         net = batch_normalization(net, scope='batch_norm')
@@ -46,7 +46,8 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, **kwarg
         assert cut_size % 2 == 0
         cut_size //= 2
         print("after conv", net.get_shape())
-        net = tf.slice(net, [0, cut_size, 0], [-1, block_size // 8, -1], name="Cutting")
+        net = tf.slice(net, [0, cut_size, 0],
+                       [-1, block_size // 8, -1], name="Cutting")
         print("after slice", net.get_shape())
 
         net = tf.transpose(net, [1, 0, 2], name="Shift_to_time_major")
@@ -58,7 +59,8 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, **kwarg
             outputs, [tf.shape(outputs), block_size // 8 * batch_size, state_size], first_n=1, message="outputs_pre_w")
         print("outputs", outputs.get_shape())
         with tf.variable_scope("Output"):
-            outputs = tf.reshape(outputs, [block_size // 8 * batch_size, state_size], name="flatten")
+            outputs = tf.reshape(
+                outputs, [block_size // 8 * batch_size, state_size], name="flatten")
             W = tf.get_variable("W", shape=[state_size, out_classes])
             b = tf.get_variable("b", shape=[out_classes])
             outputs = tf.matmul(outputs, W) + b
@@ -72,44 +74,47 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, **kwarg
     }
 
 
-def model_run(run_no, hyper):
+def model_run(run_no, experiment_id, hyper):
+    print("Running hyper parameters")
+    for k in sorted(hyper.keys()):
+        print("%-20s: %7s" % (k, str(hyper[k])))
 
     start_timestamp = monotonic()
-
     model = model_utils.Model(
         tf.Graph(),
         block_size_x=8 * 128,
         block_size_y=128,
-        in_data="RAW",
-        num_blocks=6,
-        batch_size=2**hyper['batch_size_log'],
+        in_data="ALIGNED_RAW",
+        num_blocks=1,
+        batch_size=32,
         max_reach=8 * 20,  # 160
         model_fn=model_fn,
-        queue_cap=500,
+        queue_cap=300,
         overwrite=True,
         reuse=False,
         shrink_factor=8,
-        run_id="sigopt_%02d" % run_no,
-        lr_fn=lambda global_step: tf.train.exponential_decay(hyper['initial_lr'], global_step, 100000, hyper['decay_factor']),
+        run_id="sigopt_%s_%02d" % (experiment_id, run_no),
+        lr_fn=lambda global_step: tf.train.exponential_decay(
+            hyper['initial_lr'], global_step, 100000, hyper['decay_factor']),
         hyper=hyper,
     )
 
-    model.logger.info("Running hyper parameters %s", str(hyper))
-
     try:
-        model.init_session()
-        for i in range(model.restore(must_exist=False) + 1, 50):
-            print('\r%s Step %4d, loss %7.4f' % (model.run_id, i, model.train_minibatch()), end='')
-            if i % 100 == 0:
-                model.run_validation()
+        model.init_session(num_workers=15)
+        for i in range(model.restore(must_exist=False) + 1, 100001):
+            print('\r%s Step %4d, loss %7.4f' % (model.run_id, i,
+                                                 model.train_minibatch(trace_every=500)), end='')
+            if i > 0 and i % 20 == 0:
+                model.run_validation(num_batches=1)
                 model.summarize(write_example=False)
-            if i % 3000 == 0:
+            if i > 0 and i % 3000 == 0:
                 model.save()
 
         model.save()
         model.logger.info("Running final validation run")
         avg_loss, avg_edit = model.run_validation(num_batches=10)
-        model.logger.info("Average loss %7.4f, Average edit distance %7.4f", avg_loss, avg_edit)
+        model.logger.info(
+            "Average loss %7.4f, Average edit distance %7.4f", avg_loss, avg_edit)
         return avg_edit, {
             'time[h]': (monotonic() - start_timestamp) / 3600.0,
             'logdir': model.log_dir,
@@ -123,33 +128,55 @@ def model_run(run_no, hyper):
         model.close_session()
 
 
-if __name__ == "__main__":
+params = [
+    dict(name='initial_lr', type='double',
+         bounds=dict(min=1e-5, max=1e-3)),
+    dict(name='decay_factor', type='double',
+         bounds=dict(min=1e-3, max=0.5)),
+]
 
+for layer in [1, 2, 3]:
+    params.extend([
+        {"name": "l%d_lower_lg" % layer, "type": 'int',
+            "bounds": {'min': 3, 'max': 6}},
+        {"name": "l%d_upper_lg" % layer, "type": 'int',
+            "bounds": {'min': 5, 'max': 8}},
+        {"name": "b%d_layers" % layer, "type": 'int',
+            "bounds": {'min': 2, 'max': 4}},
+        {"name": "l%d_receptive_field_l" % layer, "type": 'int',
+            "bounds": {'min': 0, 'max': 2}},
+        {"name": "l%d_receptive_field_u" % layer, "type": 'int',
+            "bounds": {'min': 1, 'max': 3}},
+    ])
+
+
+def verify_hyper(hyper):
+    for i in [1, 2, 3]:
+        if hyper['l%d_upper_lg' % i] < hyper['l%d_lower_lg' % i]:
+            print("Rejecting because upper/lower")
+            return False
+        if hyper['l%d_receptive_field_l' % i] < hyper['l%d_receptive_field_u' % i]:
+            print("Rejecting because receptive field mismatch")
+            return False
+
+    if hyper['l1_upper_lg'] > hyper['l2_upper_lg']:
+        print("Rejecting beause monotonic channel size")
+        return False
+    if hyper['l2_upper_lg'] > hyper['l3_upper_lg']:
+        print("Rejecting beause monotonic channel size")
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
     conn = Connection(client_token=os.environ["SIGOPT_KEY"])
 
     if os.environ["EXPERIMENT_ID"] == "NEW":
         experiment = conn.experiments().create(
             name='MinION basecaller',
-            parameters=[
-                dict(name='batch_size_log', type='int', bounds=dict(min=4, max=7)),
-                dict(name='initial_lr', type='double',
-                     bounds=dict(min=1e-5, max=1e-3)),
-                dict(name='decay_factor', type='double',
-                     bounds=dict(min=1e-3, max=0.5)),
-
-                dict(name="lower_lg", type='int', bounds={'min': 3, 'max': 6}),
-                dict(name="upper_lg", type='int', bounds={'min': 5, 'max': 8}),
-
-                dict(name="b1_layers", type='int', bounds={'min': 2, 'max': 4}),
-                dict(name="b2_layers", type='int', bounds={'min': 2, 'max': 4}),
-                dict(name="b3_layers", type='int', bounds={'min': 2, 'max': 4}),
-
-                dict(name="receptive_field_l", type='int',
-                     bounds={'min': 0, 'max': 2}),
-                dict(name="receptive_field_u", type='int',
-                     bounds={'min': 1, 'max': 3})
-            ],
-            observation_budget=int(os.environ["OBS"])
+            parameters=params,
+            observation_budget=30
         )
         print("Created experiment: https://sigopt.com/experiment/" + experiment.id)
         experiment_id = experiment.id
@@ -163,8 +190,22 @@ if __name__ == "__main__":
 
         suggestion = conn.experiments(experiment_id).suggestions().create()
         hyper = dict(suggestion.assignments)
-        print(hyper)
-        value, metadata = model_run(run_no, hyper)
+
+        while not verify_hyper(hyper):
+            print("Rejecting suggestion:")
+            for k in sorted(hyper.keys()):
+                print("%-20s: %7s" % (k, str(hyper[k])))
+            conn.experiments(experiment_id).observations().create(
+                suggestion=suggestion.id,
+                metadata=dict(
+                    hostname=model_utils.hostname,
+                ),
+                failed=True
+            )
+            suggestion = conn.experiments(experiment_id).suggestions().create()
+            hyper = dict(suggestion.assignments)
+
+        value, metadata = model_run(run_no, experiment_id, hyper)
         conn.experiments(experiment_id).observations().create(
             suggestion=suggestion.id,
             value=-value,
