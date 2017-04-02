baa00a33c9d4f55986bcf70d1697f26ede42d5ea
Neven Miculinic <neven.miculinic@gmail.com>
2017-04-02 23:39:20 +0200

    Adapted model_big_3 to new API.
    P.S.
    Model sucks

--- src/model_big_3.py
+++ src/model_big_3.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import numpy as np
-from util import atrous_conv1d
-import os
+from ops import atrous_conv1d
 import model_utils
 from tflearn.layers.conv import max_pool_1d
-import logging
-import ops
+from util import sigopt_double
+import input_readers
 
-def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, k, reuse=False):
+
+def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, reuse=False, **kwargs):
     """
         Args:
         net -> Input tensor shaped (batch_size, max_reach + block_size + max_reach, 3)
@@ -25,10 +25,8 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, k, reus
                             3, net.get_shape()[-1], no_channel))
                         bias = tf.get_variable("b", shape=(no_channel,))
                         net = atrous_conv1d(
-                            net, filter, i, padding="VALID") + bias
+                            net, filter, i, padding="SAME") + bias
                         net = tf.nn.relu(net)
-                net = tf.Print(net, [tf.shape(net)],
-                               first_n=5, message="net, pre_pool")
                 net = max_pool_1d(net, 2)
         print("after conv", net.get_shape())
         net = tf.transpose(net, [1, 0, 2], name="Shift_to_time_major")
@@ -39,51 +37,59 @@ def model_fn(net, X_len, max_reach, block_size, out_classes, batch_size, k, reus
         init_state = tf.constant(0.1, dtype=tf.float32)
         final_state = tf.constant(0.1, dtype=tf.float32)
 
-        outputs = tf.Print(
-            outputs, [tf.shape(outputs)], first_n=1, message="outputs_pre_w")
         print("outputs", outputs.get_shape())
         with tf.variable_scope("Output"):
-            outputs = tf.reshape(outputs, [-1, state_size])
             W = tf.get_variable("W", shape=[state_size, out_classes])
             b = tf.get_variable("b", shape=[out_classes])
-            outputs = tf.matmul(outputs, W) + b
-            logits = tf.reshape(
-                outputs, [block_size // 8, batch_size, out_classes])
+            logits = tf.nn.conv1d(outputs, tf.reshape(W, (1, state_size, out_classes)), 1, padding='SAME')
+            logits += b
     print("model out", logits.get_shape())
     return {
         'logits': logits,
         'init_state': init_state,
         'final_state': final_state,
-        'reg': k * ops.running_mean(logits, [3, 4, 5, 6, 7], [1, 2, 4, 8, 16], out_classes)
     }
 
 
-if __name__ == "__main__":
-    model = model_utils.Model(
-        tf.get_default_graph(),
-        block_size_x=8 * 50,
-        block_size_y=50,
-        in_data="RAW",
-        num_blocks=6,
+def model_setup_params(hyper):
+    print("Requesting %s hyperparams" % __file__)
+    return dict(
+        g=tf.Graph(),
+        block_size_x=8 * 3 * 600 // 2,
+        block_size_y=630,
+        in_data=input_readers.AlignedRaw(),
+        num_blocks=3,
         batch_size=16,
-        max_reach=147,
-        model_fn=model_fn,
+        max_reach=98,  # 240
         queue_cap=300,
-        overwrite=True,
-        reuse=False,
+        overwrite=False,
+        reuse=True,
         shrink_factor=8,
-        run_id=__file__[:-3],
-        hyper={'k': 2000.0}
+        dtype=tf.float32,
+        model_fn=model_fn,
+        lr_fn=lambda global_step: tf.train.exponential_decay(
+            hyper['initial_lr'], global_step, 100000, hyper['decay_factor']),
+        hyper=hyper,
     )
 
-    try:
-        model.init_session()
-        for i in range(model.restore(must_exist=False) + 1, 100001):
-            model.train_minibatch()
-            if i % 200 == 0:
-                model.run_validation()
-                model.summarize(write_example=False)
-            if i % 2000 == 0:
-                model.save()
-    finally:
-        model.close_session()
+
+params = [
+    sigopt_double('initial_lr', 1e-5, 1e-3),
+    sigopt_double('decay_factor', 1e-3, 0.5),
+]
+
+default_params = {
+    'initial_lr': 0.000965352400196344,
+    'decay_factor': 0.0017387361908150767,
+}
+
+
+if __name__ == "__main__":
+    params = default_params
+    model = model_utils.Model(
+        **model_setup_params(params)
+    )
+
+    model.init_session()
+    model.simple_managed_train_model(25000)
+    model.close_session()
