23d114d79be6420c6f5daf6186372fbd17430114
Neven Miculinic <neven.miculinic@gmail.com>
2017-04-02 23:57:46 +0200

    Karla_ensamble realistic test file

--- src/karla_ensamble.py
+++ src/karla_ensamble.py
@@ -3,11 +3,18 @@ import model_utils
 import os
 load_dotenv(find_dotenv())
 
+models = [
+    ('model_big_1', 'model_big_1'),
+    ('model_big_2', 'model_big_2'),
+    ('residual_deep', 'karla_docker/residual_deep_prod_17466_9939286'),
+    ('residual_deep', 'karla_docker/residual_deep_prod_17466_9895025'),
+    ('residual_deep', 'karla_docker/residual_deep_prod_17466_2'),
+]
 
 if __name__ == "__main__":
     model = model_utils.Ensamble(
-        ['residual_deep'],
-        [os.path.join(model_utils.repo_root, 'log', 'karla_docker', 'residual_deep_prod_17466_%d' % x) for x in [9939286]],
+        [x[0] for x in models],
+        [os.path.join(model_utils.repo_root, 'log', x[1]) for x in models],
     )
 
     model.init_session()
