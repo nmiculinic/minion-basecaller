b0e915cd1337043a2c13779cbca13a4f51867204
Neven Miculinic <neven.miculinic@gmail.com>
2017-05-07 18:08:44 +0200

    Fixed script bugs

--- mincall/tools/analyse_bc.py
+++ mincall/tools/analyse_bc.py
@@ -32,35 +32,36 @@ basecallers = {
 os.makedirs(args.out_folder, exist_ok=True)
 dfs = {}
 for name, cmd in basecallers.items():
+    logger = logging.getLogger(name)
     fasta_path = os.path.join(args.out_folder, name + ".fa")
     if os.path.isfile(fasta_path):
-        logging.info("%s exists, skipping", fasta_path)
+        logger.info("%s exists, skipping", fasta_path)
     else:
-        logging.info("Basecalling %s with %s", args.input_folder, name)
-        logging.info("Output file", fasta_path)
-        logging.info("Full command: %s", " ".join(cmd))
+        logger.info("Basecalling %s with %s", args.input_folder, name)
+        logger.info("Output file", fasta_path)
+        logger.info("Full command: %s", " ".join(cmd))
         with open(fasta_path, 'w') as f:
             subprocess.check_call(cmd, stdout=f)
 
     sam_path = os.path.join(args.out_folder, name + ".sam")
     if os.path.isfile(sam_path):
-        logging.info("%s exists, skipping", sam_path)
+        logger.info("%s exists, skipping", sam_path)
     else:
-        logging.info("Aligning %s to reference %s with graphmap", fasta_path, args.ref)
+        logger.info("Aligning %s to reference %s with graphmap", fasta_path, args.ref)
         align_utils.align_with_graphmap(fasta_path, args.ref, args.circular, sam_path)
 
     filtered_sam = os.path.join(args.out_folder, name + "_filtered.sam")
     if os.path.isfile(filtered_sam):
-        logging.info("%s exists, skipping", filtered_sam)
+        logger.info("%s exists, skipping", filtered_sam)
     else:
         filters = [read_len_filter(min_len=args.min_length)]
         n_kept, n_discarded = filter_aligments_in_sam(sam_path, filtered_sam, filters)
-        logging.info("Outputed filtered sam to %s\n%d kept, %d discarded",
+        logger.info("Outputed filtered sam to %s\n%d kept, %d discarded",
                  filtered_sam, n_kept, n_discarded)
 
     reads_csv = os.path.join(args.out_folder, name + "_read_data.csv")
     if os.path.isfile(reads_csv):
-        logging.info("%s file exists, loading", reads_csv)
+        logger.info("%s file exists, loading", reads_csv)
         df = pd.read_csv(reads_csv)
     else:
         df = error_rates_for_sam(filtered_sam)
@@ -70,17 +71,19 @@ for name, cmd in basecallers.items():
             desc.to_latex(f)
         with open(os.path.join(args.out_folder, name + "_read_summary.txt"), 'w') as f:
             desc.to_string(f)
-    logging.info("%s\n%s", name, df.describe())
+    logger.info("%s\n%s", name, df.describe())
     dfs[name] = df
 
     consensus_report_path = os.path.join(args.out_folder, name + "consensus_report.csv")
     if os.path.isfile(consensus_report_path):
         consensus_report = pd.read_csv(consensus_report_path)
+        logger.info("%s exists, loading", consensus_report_path)
     else:
         consensus_report = get_consensus_report(filtered_sam, args.ref, args.coverage_threshold)
         consensus_report.to_csv(consensus_report_path)
         with open(os.path.splitext(consensus_report_path)[0] + ".txt", 'w') as f:
             consensus_report.to_string(f)
+    logger.info("%s consensus_report:\n%s", name, consensus_report)
 
 
 fig_kde_path = os.path.join(args.out_folder, name + "reads_kde.png")
@@ -90,10 +93,10 @@ fig_kde, axes_kde = plt.subplots(3, 2)
 fig_hist, axes_hist = plt.subplots(3, 2)
 fig_kde.set_size_inches(12, 20)
 fig_hist.set_size_inches(12, 20)
-for col, ax_kde, ax_hist in zip(next(iter(dfs.values()))._get_numeric_data(), axes_kde.ravel(), axes_hist):
-    for k in df.keys():
-        sns.kdeplot(df[k][col], shade=True, label=k, alpha=0.5, ax=ax_kde)
-        plt.hist(df[k][col], label=k, alpha=0.5, ax=ax_hist)
+for col, ax_kde, ax_hist in zip(next(iter(dfs.values()))._get_numeric_data(), axes_kde.ravel(), axes_hist.ravel()):
+    for k in dfs.keys():
+        sns.kdeplot(dfs[k][col], shade=True, label=k, alpha=0.5, ax=ax_kde)
+        ax_hist.hist(dfs[k][col], label=k, alpha=0.5)
     for ax in [ax_kde, ax_hist]:
         ax.legend()
         ax.set_title(col)
