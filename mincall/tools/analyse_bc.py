468c669f9869c104d542f52a7013ecfd36c46271
Neven Miculinic <neven.miculinic@gmail.com>
2017-05-16 22:39:10 +0200

    Small update

--- mincall/tools/analyse_bc.py
+++ mincall/tools/analyse_bc.py
@@ -73,8 +73,8 @@ def albacore(name):
 basecallers = OrderedDict([
     ("albacore", albacore),
     ("mincall_m270", cmd(["nvidia-docker", "run", "--rm", "-v", "%s:/data" % args.input_folder, "-u=%d" % os.getuid(), "nmiculinic/mincall:9947283"])),
-    ("nanonet", cmd(["nanonetcall", args.input_folder, "--chemistry", "r9", "--jobs", "24"])),
     ('metrichorn', cmd(["poretools", "fastq", "--type", "fwd", args.input_folder], ext='fastq'))
+    ("nanonet", cmd(["nanonetcall", args.input_folder, "--chemistry", "r9", "--jobs", "24"])),
 ])
 
 consensus_reports = []
