1100fb22c99dd47752e342c1f1c25b30429a76ff
Neven Miculinic <neven.miculinic@gmail.com>
2017-06-21 17:25:20 +0200

    Initial gapped implementation

--- mincall/tools/align_ref.cpp
+++ mincall/tools/align_ref.cpp
@@ -10,10 +10,13 @@ clang++ -std=c++11 mincall/tools/align_ref.cpp -I ~/Desktop/HighFive/include -lh
 #include <highfive/H5File.hpp>
 #include <boost/multi_array.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
 #include <string>
+#include <stdexcept>
 #include <fstream>
 #include <cctype>
 #include <map>
+
 
 using namespace HighFive;
 using namespace boost;
@@ -46,6 +49,7 @@ std::ostream& operator<<(std::ostream& stream,
 
 
 const std::string DATASET_NAME("/Analyses/MinCall/Logits");
+const int GAP_WIDTH=10;
 
 std::map<char, int> n2i {
     {'A', 0},
@@ -74,43 +78,69 @@ multi_array<int, 1> read_ref(const std::string ref_file) {
     return refe;
 }
 
+bool test_valid_path(const multi_array<int, 1>& path, const multi_array<int, 1>& ref) {
+    if (path[0] != ref[0]){
+        return false;
+    }
+
+    int ref_pos = 0;
+    for (int i = 0; i < path.size(); ++i) {
+        if (path[i] != ref[ref_pos]) {
+            if (path[i] != ref[++ref_pos]) {
+                return false;
+            }
+        }
+    }
+    return ref_pos == ref.size() - 1;
+}
+
 void calc_transition_matrix(const multi_array<int, 1>& ref, const multi_array<double, 2>& logits) {
     multi_array<double, 2> sol(extents[ref.shape()[0]][logits.shape()[0]]);
+    fill(sol.origin(), sol.origin() + sol.size(), -std::numeric_limits<double>::infinity());
     multi_array<int, 2>   back(extents[ref.shape()[0]][logits.shape()[0]]); // For backtracing dp matrix
+    multi_array<int, 1>   center(extents[ref.shape()[0]]); // For knowing center line
 
     sol[0][0] = logits[0][ref[0]];
     back[0][0] = -1;
 
-    // We're at first place on reference, forbidden
-    for(int j = 1; j < logits.shape()[0]; ++j) {
-        sol[0][j] = -1e9;
-        back[0][j] = -1;
-    }
+    auto proxy = [&center](multi_array<double, 2>&arr, int i, int j)->double& {return arr[i][j - center[i] + GAP_WIDTH];};
 
+    auto proxyI = [&center](multi_array<int, 2>&arr, int i, int j)->int& {return arr[i][j - center[i] + GAP_WIDTH];};
+
+    center[0] = 0;
     for (int i = 1; i < ref.shape()[0]; ++i){
         // Enforcing i <= j, since we must always move by one on reference
         // That is we disallow transition where we stay in same position on logits and move on reference. In other words each logit member has one and only one alignemtn on reference in dp table.
-
-        for (int j = i; j < logits.shape()[0]; ++j) {
+        center[i] = i * double(logits.shape()[0] - 1) / (ref.shape()[0] - 1);
+        for (int j = max(i, center[i] - GAP_WIDTH);
+                 j <= min((int) logits.shape()[0] - 1, center[i] + GAP_WIDTH);
+            ++j) {
 
             // case we move on both logits and reference
-            auto mov = sol[i - 1][j - 1] + logits[j][ref[i]];
-            sol[i][j] = mov;
-            back[i][j] = i - 1;
+            if (center[i - 1] - GAP_WIDTH <= j - 1 && j - 1 <= center[i - 1] + GAP_WIDTH) {
+                proxy(sol, i, j) = proxy(sol, i - 1, j - 1) + logits[j][ref[i]];
+                proxyI(back, i, j) = i - 1;
+                cout << format("MOV (%1%, %2%) <- (%3%, %4%)") % i % j % (i-1) % (j-1);
+            }
+
 
             // case We move on logits, but not on reference
-            if (i <= j - 1) {
-                auto same = sol[i][j - 1] +
-                log(
-                    exp(logits[j][ref[i]]) +
-                    exp(logits[j][8])
-                );
+            if (i <= j - 1 && center[i] - GAP_WIDTH <= j - 1 && j - 1 <= center[i] + GAP_WIDTH) {
+                auto same = proxy(sol, i, j - 1) +
+                // log(
+                    // exp(logits[j][ref[i]]) +
+                    // exp(logits[j][8])
+                // );
+                logits[j][ref[i]];
                 // 8 is blank label.. This should properly add two probabilites together, I hope, without needing to renormalize.
-                if (same > sol[i][j]) {
-                    sol[i][j] = same;
-                    back[i][j] = i;
+                // It's speed improvement without additional factoring
+
+                if (same > proxy(sol, i, j)) {
+                    proxy(sol, i, j)  = same;
+                    proxyI(back, i, j) = i;
                 }
             }
+            cout << " " << proxyI(back, i, j) << ", " << j - 1 << endl;
         }
     }
 
@@ -118,11 +148,18 @@ void calc_transition_matrix(const multi_array<int, 1>& ref, const multi_array<do
     int ref_pos = ref.shape()[0] - 1;
     for (int log_pos = logits.shape()[0] - 1; log_pos >= 0; --log_pos) {
         path[log_pos] = ref[ref_pos];
-        cout << ref_pos - back[ref_pos][log_pos];
-        ref_pos = back[ref_pos][log_pos];
-        // std::cout << "(" << log_pos << ", " << ref_pos << "), ";
+        cout << ref_pos - proxyI(back, ref_pos, log_pos);
+        ref_pos = proxyI(back, ref_pos, log_pos);
+        std::cout << "(" << ref_pos << ", " << log_pos << ")" << endl;
     }
+
+
     cout << endl << path;
+    cout << center << endl;
+    cout << "Is Path valid: " << test_valid_path(path, ref);
+    if (!test_valid_path(path, ref)) {
+        throw std::runtime_error("Invalid path found, internal error!");
+    }
 }
 
 void process(const std::string fast5_file) {
