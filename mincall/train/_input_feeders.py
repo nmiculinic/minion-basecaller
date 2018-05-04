fea368c5528b4a5dcb2a37305bdb586e8e1f3219
Neven Miculinic <neven.miculinic@gmail.com>
2018-05-04 15:02:24 +0200

    Some fooling around

--- mincall/train/_input_feeders.py
+++ mincall/train/_input_feeders.py
@@ -4,6 +4,8 @@ import voluptuous
 import os
 import random
 import gzip
+import numpy as np
+from multiprocessing import Queue
 
 class InputFeederCfg(NamedTuple):
     batch_size: int
@@ -16,11 +18,35 @@ class InputFeederCfg(NamedTuple):
             'seq_length': int,
         })(data))
 
-def produce_datapoints(cfg: InputFeederCfg, fnames: List[str]):
+def produce_datapoints(cfg: InputFeederCfg, fnames: List[str], q: Queue):
+    # TODO: Check correctness
     random.seed(os.urandom(20))
     random.shuffle(fnames)
     for x in fnames:
         with gzip.open(x, "r") as f:
             dp = dataset_pb2.DataPoint()
             dp.ParseFromString(f.read())
-            print(dp)
+
+            signal = np.array(dp.signal, dtype=np.float32)
+            buff = np.zeros(cfg.seq_length, dtype=np.int32)
+
+            basecall_idx = 0
+            basecall_squiggle_idx = 0
+            for start in range(0, len(signal), cfg.seq_length):
+                buff_idx = 0
+                while basecall_idx < len(dp.basecalled) and dp.lower_bound[basecall_idx] < start + cfg.seq_length:
+                    while basecall_squiggle_idx < len(dp.basecalled_squiggle) and dp.basecalled_squiggle[basecall_squiggle_idx] == dataset_pb2.BLANK: # Find first non-blank basecall_squiggle
+                        basecall_squiggle_idx += 1
+
+                    if basecall_squiggle_idx >= len(dp.basecalled_squiggle):
+                        break
+                    else:
+                        buff[buff_idx] = dp.basecalled_squiggle[basecall_squiggle_idx]
+                        buff_idx+=1
+                        basecall_squiggle_idx +=1
+                        basecall_idx += 1
+                q.put([
+                    signal[start:start + cfg.seq_length],
+                    np.copy(buff[:buff_idx]),
+                ])
+
--- mincall/train/_train.py
+++ mincall/train/_train.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import voluptuous
 import sys
 import yaml
@@ -8,6 +9,11 @@ from voluptuous.humanize import humanize_error
 from glob import glob
 from pprint import pformat, pprint
 from ._input_feeders import InputFeederCfg, produce_datapoints
+from multiprocessing import Manager, Process, Queue
+import tensorflow as tf
+import queue
+from mincall import dataset_pb2
+
 import toolz
 from tqdm import tqdm
 
@@ -53,7 +59,49 @@ def run(cfg: TrainConfig):
         batch_size=10,
         seq_length=10,
     )
-    produce_datapoints(input_feeder_cfg, datapoints)
+
+    m = Manager()
+    q: Queue = m.Queue()
+
+    p = Process(target=produce_datapoints, args=(input_feeder_cfg, datapoints, q))
+    p.start()
+    p.join()
+
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+
+    indices = tf.placeholder(dtype=tf.int64)
+    values = tf.placeholder(dtype=tf.int32)
+    dense_shape = tf.placeholder(dtype=tf.int64)
+    st = tf.SparseTensor(
+        indices=indices,
+        values=values,
+        dense_shape=dense_shape,
+    )
+    c = tf.sparse_concat(0, [st,st], expand_nonconcat_dim=True)
+    with tf.train.MonitoredSession(session_creator=tf.train.ChiefSessionCreator(config=config)) as sess:
+        while True:
+            try:
+                signal, labels = q.get(timeout=0.5)
+                v = tf.SparseTensorValue(
+                    indices=np.array(np.vstack([np.zeros(len(labels)), np.arange(len(labels))]).transpose(), dtype=np.int32),
+                    values=labels,
+                    dense_shape=[1, len(labels)],
+                )
+                x = sess.run(c, feed_dict={
+                    indices: v.indices,
+                    values:  v.values,
+                    dense_shape: v.dense_shape,
+                })
+
+                print(v)
+                print(x)
+                if len(labels) > 0:
+                    break
+            except queue.Empty:
+                break
+            except Exception as e:
+                print(type(e).__name__, type(e))
 
 
 def run_args(args):
