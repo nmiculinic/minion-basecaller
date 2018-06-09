40aaecbc1184054c05ec539b78185f5283a05528
Neven Miculinic <neven.miculinic@gmail.com>
2018-06-09 21:17:22 +0200

    Added chiron_like model

--- mincall/train/models.py
+++ mincall/train/models.py
@@ -144,9 +144,41 @@ def m270(n_classes: int, hparams: str):
     return models.Model(inputs=[input], outputs=[net]), 2**len(block_channels)
 
 
+def chiron_like(n_classes: int, hparams: str):
+    input = layers.Input(shape=(None, 1))
+    net = input # (batch_size, sequence_len, channels)
+    out_chan = 256
+    for i in range(3):
+        with tf.name_scope(f"block_{i}"):
+            net = layers.BatchNormalization()(net)
+            with tf.variable_scope('branch1'):
+                b1 = layers.Conv1D(filters=out_chan, kernel_size=1, padding='same', use_bias=False)(net)
+            with tf.variable_scope('branch2'):
+                b2 = net
+                b2 = layers.Conv1D(filters=out_chan, kernel_size=1, padding='same', use_bias=False)(b2)
+                b2 = layers.Conv1D(filters=out_chan, kernel_size=3, padding='same', use_bias=False)(b2)
+                b2 = layers.Conv1D(filters=out_chan, kernel_size=1, padding='same', use_bias=False)(b2)
+            with tf.variable_scope('plus'):
+                net = layers.Add()([b1, b2])
+                net = layers.Activation('relu')(net)
+
+    hidden_num = [100, 100, n_classes]
+    # RNN layers:
+    if tf.test.is_built_with_cuda():
+        logging.getLogger(__name__).info(f"Using CuDNNLSTM optimized cell")
+        cell = layers.CuDNNLSTM
+    else:
+        cell = layers.LSTM
+
+    for h in hidden_num:
+        net = cell(h, return_sequences=True)(net)
+    return models.Model(inputs=[input], outputs=[net]), 1
+
+
 all_models: Dict[str, Callable[[str], models.Model]] = {
     'dummy': dummy_model,
     'big_01': big_01,
     'm270': m270,
+    'chiron': chiron_like,
 }
 
