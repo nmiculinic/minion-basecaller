0b60501eda759387dfb587af6e662f8b34a9ac97
Neven Miculinic <neven.miculinic@gmail.com>
2018-06-17 23:59:08 +0200

    Raise error on cancelled and not by coord

--- mincall/train/_input_feeders.py
+++ mincall/train/_input_feeders.py
@@ -232,6 +232,7 @@ class DataQueue():
                                     return
                                 else:
                                     self.logger.error("Cancelled error occurred yet coord is not stopping!", exc_info=True)
+                                    raise
                             except tf.errors.DeadlineExceededError:
                                 self.logger.warning("Queue pushing timeout exceeded")
                             if i > 0 and i % 2000 == 0:
